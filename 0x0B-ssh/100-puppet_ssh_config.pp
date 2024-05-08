b8ed9c72cd979dfaf79afbbd352a592210fa00db
Stephani6G <stephaniegloria283@gmail.com>
2024-05-08 04:36:50 +0000

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# User Puppet to make changes to our client-side ssh config file
+# to allow us to connect to the server without password
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
