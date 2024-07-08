e14e4c20a69861a3737996baa51c2fab9bafe4f1
Stephani6G <stephaniegloria283@gmail.com>
2024-07-08 16:34:55 +0000

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-
-"""
-Extend the Python script in above tasks to export data in the JSON format
-"""
+""" JSON Get records for all employees and export to JSON"""
 
 import json
 import requests
 from sys import argv
 
 
-if __name__ == '__main__':
-    users_url = 'https://jsonplaceholder.typicode.com/users'
-    tasks_url = 'https://jsonplaceholder.typicode.com/todos'
-
-    users = requests.get(users_url).json()
-    tasks = requests.get(tasks_url).json()
-
-    todo_all_employees = {}
+if __name__ == "__main__":
+    """Records all tasks from all employees"""
+    url = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get(url).json()
+    all_dict = {}
 
     for user in users:
-        user_id = user.get('id')
-        username = user.get('username')
-        todo_all_employees[user_id] = []
+        user_id = user.get("id")
+        url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            user_id)
+        todos = requests.get(url).json()
+        task_list = []
 
-        for task in tasks:
-            if task.get('userId') == user_id:
-                todo = {
-                    'username': username,
-                    'task': task.get('title'),
-                    'completed': task.get('completed'),
-                }
-                todo_all_employees[user_id].append(todo)
+        for task in todos:
+            task_dict = {}
+            task_dict["username"] = user.get("username")
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            task_list.append(task_dict)
 
-    with open('todo_all_employees.json', mode='w') as file:
-        json.dump(todo_all_employees, file)
+        all_dict[user_id] = task_list
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(all_dict, jsonfile)
