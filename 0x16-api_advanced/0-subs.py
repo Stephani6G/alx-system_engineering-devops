c61787dc13e25eefb40ec4db24400164c45c0e09
Stephani6G <stephaniegloria283@gmail.com>
2024-09-15 21:03:03 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-
 """
-Queries the Reddit API and returns the number of total subscribers)
- for a given subreddit
+Queries the Reddit API and returns the number of subscribers
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """Returns the number of subscribers for a given subreddit"""
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data', {}).get('subscribers', 0)
+    else:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'myBot/0.0.1'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
-
